d338a137f6148290f983e93d34ef061114e5920d
iigorh <igorhenriquepps@gmail.com>
2023-09-25 16:57:17 -0300

    método para exibição na página 'lista' refeito

--- src/main/java/br/com/igor/projetosenai/controller/DespesasController.java
+++ src/main/java/br/com/igor/projetosenai/controller/DespesasController.java
@@ -2,8 +2,9 @@ package br.com.igor.projetosenai.controller;
 
 
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import javax.validation.Valid;
 
@@ -11,12 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import br.com.igor.projetosenai.model.Despesas;
 import br.com.igor.projetosenai.repository.DespesaRepository;
@@ -44,91 +42,82 @@ public class DespesasController {
 	List<Despesas> despesas = repositorio.findAll();
 	model.addAttribute("despesas", despesas);
 
-	List<Double> valoresJaneiro04 = despesas.stream().map((Despesas despesa) -> despesa.getJan_nd04()).collect(Collectors.toList());
-	List<Double> valoresFevereiro04 = despesas.stream().map((Despesas despesa) -> despesa.getFev_nd04()).collect(Collectors.toList());
-	List<Double> valoresMarco04 = despesas.stream().map((Despesas despesa) -> despesa.getMar_nd04()).collect(Collectors.toList());
-	List<Double> valoresAbril04 = despesas.stream().map((Despesas despesa) -> despesa.getAbr_nd04()).collect(Collectors.toList());
-	List<Double> valoresMaio04 = despesas.stream().map((Despesas despesa) -> despesa.getMai_nd04()).collect(Collectors.toList());
-	List<Double> valoresJunho04 = despesas.stream().map((Despesas despesa) -> despesa.getJun_nd04()).collect(Collectors.toList());
-	List<Double> valoresJulho04 = despesas.stream().map((Despesas despesa) -> despesa.getJul_nd04()).collect(Collectors.toList());
-	List<Double> valoresAgosto04 = despesas.stream().map((Despesas despesa) -> despesa.getAgo_nd04()).collect(Collectors.toList());
-	List<Double> valoresSetembro04 = despesas.stream().map((Despesas despesa) -> despesa.getSet_nd04()).collect(Collectors.toList());
-	List<Double> valoresOutubro04 = despesas.stream().map((Despesas despesa) -> despesa.getOut_nd04()).collect(Collectors.toList());
-	List<Double> valoresNovembro04 = despesas.stream().map((Despesas despesa) -> despesa.getNov_nd04()).collect(Collectors.toList());
-	List<Double> valoresDezembro04 = despesas.stream().map((Despesas despesa) -> despesa.getDez_nd04()).collect(Collectors.toList());
-	model.addAttribute("valorJan04", valoresJaneiro04);
-	model.addAttribute("valorFev04", valoresFevereiro04);
-	model.addAttribute("valorMar04", valoresMarco04);
-	model.addAttribute("valorAbr04", valoresAbril04);
-	model.addAttribute("valorMai04", valoresMaio04);
-	model.addAttribute("valorJun04", valoresJunho04);
-	model.addAttribute("valorJul04", valoresJulho04);
-	model.addAttribute("valorAgo04", valoresAgosto04);
-	model.addAttribute("valorSet04", valoresSetembro04);
-	model.addAttribute("valorOut04", valoresOutubro04);
-	model.addAttribute("valorNov04", valoresNovembro04);
-	model.addAttribute("valorDez04", valoresDezembro04);
 	
-		
-	List<Double> valoresJaneiro30 = despesas.stream().map((Despesas despesa) -> despesa.getJan_nd30()).collect(Collectors.toList());
-	List<Double> valoresFevereiro30 = despesas.stream().map((Despesas despesa) -> despesa.getFev_nd30()).collect(Collectors.toList());
-	List<Double> valoresMarco30 = despesas.stream().map((Despesas despesa) -> despesa.getMar_nd30()).collect(Collectors.toList());
-	List<Double> valoresAbril30 = despesas.stream().map((Despesas despesa) -> despesa.getAbr_nd30()).collect(Collectors.toList());
-	List<Double> valoresMaio30 = despesas.stream().map((Despesas despesa) -> despesa.getMai_nd30()).collect(Collectors.toList());
-	List<Double> valoresJunho30 = despesas.stream().map((Despesas despesa) -> despesa.getJun_nd30()).collect(Collectors.toList());
-	List<Double> valoresJulho30 = despesas.stream().map((Despesas despesa) -> despesa.getJul_nd30()).collect(Collectors.toList());
-	List<Double> valoresAgosto30 = despesas.stream().map((Despesas despesa) -> despesa.getAgo_nd30()).collect(Collectors.toList());
-	List<Double> valoresSetembro30 = despesas.stream().map((Despesas despesa) -> despesa.getSet_nd30()).collect(Collectors.toList());
-	List<Double> valoresOutubro30 = despesas.stream().map((Despesas despesa) -> despesa.getOut_nd30()).collect(Collectors.toList());
-	List<Double> valoresNovembro30 = despesas.stream().map((Despesas despesa) -> despesa.getNov_nd30()).collect(Collectors.toList());
-	List<Double> valoresDezembro30 = despesas.stream().map((Despesas despesa) -> despesa.getDez_nd30()).collect(Collectors.toList());
-	model.addAttribute("valorJan30", valoresJaneiro30);
-	model.addAttribute("valorFev30", valoresFevereiro30);
-	model.addAttribute("valorMar30", valoresMarco30);
-	model.addAttribute("valorAbr30", valoresAbril30);
-	model.addAttribute("valorMai30", valoresMaio30);
-	model.addAttribute("valorJun30", valoresJunho30);
-	model.addAttribute("valorJul30", valoresJulho30);
-	model.addAttribute("valorAgo30", valoresAgosto30);
-	model.addAttribute("valorSet30", valoresSetembro30);
-	model.addAttribute("valorOut30", valoresOutubro30);
-	model.addAttribute("valorNov30", valoresNovembro30);
-	model.addAttribute("valorDez30", valoresDezembro30);
+	List<Double> valoresJaneiro = new ArrayList<>();
+	List<Double> valoresFevereiro = new ArrayList<>();
+	List<Double> valoresMarco = new ArrayList<>();
+	List<Double> valoresAbril = new ArrayList<>();
+	List<Double> valoresMaio = new ArrayList<>();
+	List<Double> valoresJunho = new ArrayList<>();
+	List<Double> valoresJulho = new ArrayList<>();
+	List<Double> valoresAgosto = new ArrayList<>();
+	List<Double> valoresSetembro = new ArrayList<>();
+	List<Double> valoresOutubro = new ArrayList<>();
+	List<Double> valoresNovembro = new ArrayList<>();
+	List<Double> valoresDezembro = new ArrayList<>();
 	
+	 for (Despesas despesa : despesas) {
+	        valoresJaneiro.add(despesa.getJan_nd04() + despesa.getJan_nd30() + despesa.getJan_nd33() + despesa.getJan_nd36() +
+	            despesa.getJan_nd39() + despesa.getJan_nd40() + despesa.getJan_nd52());
+	 }
+	        
+	 for (Despesas despesa : despesas) {
+	        valoresFevereiro.add(despesa.getFev_nd04() + despesa.getFev_nd30() + despesa.getFev_nd33() + despesa.getFev_nd36() +
+	             despesa.getFev_nd39() + despesa.getFev_nd40() + despesa.getFev_nd52());
+	 }
 	
-	
-	
-	
-	// Calcular o valor total para cada categoria (ND04, ND30, ND33, ND36, ND39, ND40, ND52) em todos os meses (janeiro a dezembro)
-	double valorTotalJaneiro = despesas.stream()
-	    .mapToDouble(despesa -> despesa.getJan_nd04() + despesa.getJan_nd30() + despesa.getJan_nd33() + despesa.getJan_nd36() +
-	                         despesa.getJan_nd39() + despesa.getJan_nd40() + despesa.getJan_nd52())
-	    .sum();
+	 for (Despesas despesa : despesas) {
+		    valoresMarco.add(despesa.getMar_nd04() + despesa.getMar_nd30() + despesa.getMar_nd33() + despesa.getMar_nd36() +
+		         despesa.getMar_nd39() + despesa.getMar_nd40() + despesa.getMar_nd52()); 
+	 }
+		    
+	 for (Despesas despesa : despesas) {
+		       valoresAbril.add(despesa.getAbr_nd04() + despesa.getAbr_nd30() + despesa.getAbr_nd33() + despesa.getAbr_nd36() +
+				 despesa.getAbr_nd39() + despesa.getAbr_nd40() + despesa.getAbr_nd52());
+	 }
+				    
+	 for (Despesas despesa : despesas) {
+			   valoresMaio.add(despesa.getMai_nd04() + despesa.getMai_nd30() + despesa.getMai_nd33() + despesa.getMai_nd36() +
+			    despesa.getMai_nd39() + despesa.getMai_nd40() + despesa.getMai_nd52());
+	 }
+					    
+	 for (Despesas despesa : despesas) {
+			   valoresJunho.add(despesa.getJun_nd04() + despesa.getJun_nd30() + despesa.getJun_nd33() + despesa.getJun_nd36() +
+				 despesa.getJun_nd39() + despesa.getJun_nd40() + despesa.getJun_nd52());
+	 }
+						    
+	 for (Despesas despesa : despesas) {
+			   valoresJulho.add(despesa.getJul_nd04() + despesa.getJul_nd30() + despesa.getJul_nd33() + despesa.getJul_nd36() +
+				 despesa.getJul_nd39() + despesa.getJul_nd40() + despesa.getJul_nd52());
+	 }
+							    
+	 for (Despesas despesa : despesas) {
+			   valoresAgosto.add(despesa.getAgo_nd04() + despesa.getAgo_nd30() + despesa.getAgo_nd33() + despesa.getAgo_nd36() +
+				  despesa.getAgo_nd39() + despesa.getAgo_nd40() + despesa.getAgo_nd52());
+	 }
+			   
+	for (Despesas despesa : despesas) {
+				   valoresSetembro.add(despesa.getSet_nd04() + despesa.getSet_nd30() + despesa.getSet_nd33() + despesa.getSet_nd36() +
+					  despesa.getSet_nd39() + despesa.getSet_nd40() + despesa.getSet_nd52());
+	}
+				   
+	for (Despesas despesa : despesas) {
+			   valoresOutubro.add(despesa.getOut_nd04() + despesa.getOut_nd30() + despesa.getOut_nd33() + despesa.getOut_nd36() +
+				 despesa.getOut_nd39() + despesa.getOut_nd40() + despesa.getOut_nd52());
+	}
 
-	double valorTotalFevereiro = despesas.stream()
-	    .mapToDouble(despesa -> despesa.getFev_nd04() + despesa.getFev_nd30() + despesa.getFev_nd33() + despesa.getFev_nd36() +
-	                         despesa.getFev_nd39() + despesa.getFev_nd40() + despesa.getFev_nd52())
-	    .sum();
-
-	double valorTotalMarco = despesas.stream()
-	    .mapToDouble(despesa -> despesa.getMar_nd04() + despesa.getMar_nd30() + despesa.getMar_nd33() + despesa.getMar_nd36() +
-	                         despesa.getMar_nd39() + despesa.getMar_nd40() + despesa.getMar_nd52())
-	    .sum();
-
-	double valorTotalAbril = despesas.stream()
-		    .mapToDouble(despesa -> despesa.getAbr_nd04() + despesa.getAbr_nd30() + despesa.getAbr_nd33() + despesa.getAbr_nd36() +
-		                         despesa.getAbr_nd39() + despesa.getAbr_nd40() + despesa.getAbr_nd52())
-		    .sum();
-
-    	
-// Calcular o valor total das despesas
-	double valorTotal = despesas.stream().mapToDouble(Despesas::getValor).sum();
-	model.addAttribute("valorTotal", valorTotal);
-	return "lista";	
-		}		
-		
-	
-	
+    for (Despesas despesa : despesas) {
+			  valoresNovembro.add(despesa.getNov_nd04() + despesa.getNov_nd30() + despesa.getNov_nd33() + despesa.getNov_nd36() +
+			 despesa.getNov_nd39() + despesa.getNov_nd40() + despesa.getNov_nd52());
+    }
+				   
+	for (Despesas despesa : despesas) {
+			   valoresDezembro.add(despesa.getDez_nd04() + despesa.getDez_nd30() + despesa.getDez_nd33() + despesa.getDez_nd36() +
+				 despesa.getDez_nd39() + despesa.getDez_nd40() + despesa.getDez_nd52());
+	}
+			
+	return "lista";
+	}
 	
 	
 	
