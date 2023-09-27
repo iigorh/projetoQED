6ea541c475e1ceabfc4a880ee7ec4cf00c7487c0
iigorh <igorhenriquepps@gmail.com>
2023-09-27 15:36:09 -0300

    config do Spring Boot Security, criando autorização role-based

---
+++ src/main/java/br/com/igor/projetosenai/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.com.igor.projetosenai.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.igor.projetosenai.model.User;
+
+public interface UsuarioRepository extends Repository<User, Long> {
+
+	@Query("SELEC u from User u Where u.username = : username")
+	public User getUserByUsername(@Param("username") String username);
+	
+	
+}
