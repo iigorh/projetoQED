829b197d9df9e2a0a95502c4f91a2d24f0503c4d
iigorh <igorhenriquepps@gmail.com>
2023-10-03 17:02:51 -0300

    configurado o acesso ao h2-console. Necessita configurar permissões para acesso à outras paginas

--- src/main/java/br/com/igor/projetosenai/security/WebSecurityConfig.java
+++ src/main/java/br/com/igor/projetosenai/security/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package br.com.igor.projetosenai.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +30,7 @@ public class WebSecurityConfig{
 		return new BCryptPasswordEncoder();
 	}
 	
+	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvicer() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -36,36 +40,48 @@ public class WebSecurityConfig{
 	}
 	
 	
-		
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-			auth.authenticationProvider(authenticationProvicer());
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(authenticationProvicer());
 		}
-	    public void configure(HttpSecurity http) throws Exception {
-	      
-	    	http
-	   
-	    		   .authorizeHttpRequests(auth -> auth
-	        		.requestMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
-	                .requestMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
-	                .requestMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
-	                .requestMatchers("/delete/**").hasAnyAuthority("ADMIN")
-	                .requestMatchers("/h2-console/**").permitAll()
-	                .anyRequest().authenticated()
-	                        )
-	    		   
-	    		 
-	                .formLogin().permitAll()
-	                .and()
-	                .logout().permitAll()
-	                .and()
-	        		.exceptionHandling().accessDeniedPage("/403")
-	               ;
-	    	
-	    	http.csrf().disable();
-	    	http.headers().frameOptions().disable();
-
-	               
+		
+	 @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        http
+	                .authorizeHttpRequests(auth -> auth
+	                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+	                )
+	                .headers(headers -> headers.frameOptions().disable())
+	                .csrf(csrf -> csrf
+	                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
+	        return http.build();
 	    }
+		
+	 protected void configure(HttpSecurity http) throws Exception {
+		      
+		    	http
+		    		   .authorizeHttpRequests(auth -> auth
+		    				   
+		    			.requestMatchers("/public/**", "/resources/**", "/login", "/signup").permitAll()
+		        		.requestMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
+		                .requestMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
+		                .requestMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+		                .requestMatchers("/delete/**").hasAnyAuthority("ADMIN")
+		                
+		                .requestMatchers("/h2-console/**").permitAll()
+		                .anyRequest().authenticated()
+		                )
+		                .formLogin().permitAll()
+		                .and()
+		                .logout().permitAll()
+		                .and()
+		                .exceptionHandling().accessDeniedPage("/403");		                
+		              
+		    	http.csrf(csrf -> csrf.disable());
+		    	http.headers().frameOptions().sameOrigin();
+		    	
+		    }
+		
+	 
 	 	 
 
 }
