a0f46742188171f5986590da8029d0ae2e01f2fa
iigorh <igorhenriquepps@gmail.com>
2024-04-02 15:13:38 -0300

    refactor; fix; style: conserto da função de editar; 'bifurcação' da página lista, sendo 1 para visualizar e 1 para editar agora; alteração dos nomes da navbar.

--- src/main/java/br/com/igor/projetosenai/controller/DespesasController.java
+++ src/main/java/br/com/igor/projetosenai/controller/DespesasController.java
@@ -28,7 +28,93 @@ public class DespesasController {
 	@Autowired
 	private DespesaRepository repositorio;
 
-	
+	//tabela de lista
+		@GetMapping("/lista2")
+		public String editarlista(Model model) {
+		List<Despesas> despesas = repositorio.findAll();
+		model.addAttribute("despesas", despesas);
+		
+		
+		
+		List<Double> valoresJaneiro = new ArrayList<>();
+		List<Double> valoresFevereiro = new ArrayList<>();
+		List<Double> valoresMarco = new ArrayList<>();
+		List<Double> valoresAbril = new ArrayList<>();
+		List<Double> valoresMaio = new ArrayList<>();
+		List<Double> valoresJunho = new ArrayList<>();
+		List<Double> valoresJulho = new ArrayList<>();
+		List<Double> valoresAgosto = new ArrayList<>();
+		List<Double> valoresSetembro = new ArrayList<>();
+		List<Double> valoresOutubro = new ArrayList<>();
+		List<Double> valoresNovembro = new ArrayList<>();
+		List<Double> valoresDezembro = new ArrayList<>();
+		
+		 for (Despesas despesa : despesas) {
+		        valoresJaneiro.add(despesa.getJan_nd04() + despesa.getJan_nd30() + despesa.getJan_nd33() + despesa.getJan_nd36() +
+		            despesa.getJan_nd39() + despesa.getJan_nd40() + despesa.getJan_nd52());
+		 }
+		        
+		 for (Despesas despesa : despesas) {
+		        valoresFevereiro.add(despesa.getFev_nd04() + despesa.getFev_nd30() + despesa.getFev_nd33() + despesa.getFev_nd36() +
+		             despesa.getFev_nd39() + despesa.getFev_nd40() + despesa.getFev_nd52());
+		 }
+		
+		 for (Despesas despesa : despesas) {
+			    valoresMarco.add(despesa.getMar_nd04() + despesa.getMar_nd30() + despesa.getMar_nd33() + despesa.getMar_nd36() +
+			         despesa.getMar_nd39() + despesa.getMar_nd40() + despesa.getMar_nd52()); 
+		 }
+			    
+		 for (Despesas despesa : despesas) {
+			       valoresAbril.add(despesa.getAbr_nd04() + despesa.getAbr_nd30() + despesa.getAbr_nd33() + despesa.getAbr_nd36() +
+					 despesa.getAbr_nd39() + despesa.getAbr_nd40() + despesa.getAbr_nd52());
+		 }
+					    
+		 for (Despesas despesa : despesas) {
+				   valoresMaio.add(despesa.getMai_nd04() + despesa.getMai_nd30() + despesa.getMai_nd33() + despesa.getMai_nd36() +
+				    despesa.getMai_nd39() + despesa.getMai_nd40() + despesa.getMai_nd52());
+		 }
+						    
+		 for (Despesas despesa : despesas) {
+				   valoresJunho.add(despesa.getJun_nd04() + despesa.getJun_nd30() + despesa.getJun_nd33() + despesa.getJun_nd36() +
+					 despesa.getJun_nd39() + despesa.getJun_nd40() + despesa.getJun_nd52());
+		 }
+							    
+		 for (Despesas despesa : despesas) {
+				   valoresJulho.add(despesa.getJul_nd04() + despesa.getJul_nd30() + despesa.getJul_nd33() + despesa.getJul_nd36() +
+					 despesa.getJul_nd39() + despesa.getJul_nd40() + despesa.getJul_nd52());
+		 }
+								    
+		 for (Despesas despesa : despesas) {
+				   valoresAgosto.add(despesa.getAgo_nd04() + despesa.getAgo_nd30() + despesa.getAgo_nd33() + despesa.getAgo_nd36() +
+					  despesa.getAgo_nd39() + despesa.getAgo_nd40() + despesa.getAgo_nd52());
+		 }
+				   
+		for (Despesas despesa : despesas) {
+					   valoresSetembro.add(despesa.getSet_nd04() + despesa.getSet_nd30() + despesa.getSet_nd33() + despesa.getSet_nd36() +
+						  despesa.getSet_nd39() + despesa.getSet_nd40() + despesa.getSet_nd52());
+		}
+					   
+		for (Despesas despesa : despesas) {
+				   valoresOutubro.add(despesa.getOut_nd04() + despesa.getOut_nd30() + despesa.getOut_nd33() + despesa.getOut_nd36() +
+					 despesa.getOut_nd39() + despesa.getOut_nd40() + despesa.getOut_nd52());
+		}
+
+	    for (Despesas despesa : despesas) {
+				  valoresNovembro.add(despesa.getNov_nd04() + despesa.getNov_nd30() + despesa.getNov_nd33() + despesa.getNov_nd36() +
+				 despesa.getNov_nd39() + despesa.getNov_nd40() + despesa.getNov_nd52());
+	    }
+					   
+		for (Despesas despesa : despesas) {
+				   valoresDezembro.add(despesa.getDez_nd04() + despesa.getDez_nd30() + despesa.getDez_nd33() + despesa.getDez_nd36() +
+					 despesa.getDez_nd39() + despesa.getDez_nd40() + despesa.getDez_nd52());
+		}
+		
+		
+		 
+		return "lista2";
+				
+		
+		}	
 
 	
 //tabela de lista
@@ -37,17 +123,7 @@ public class DespesasController {
 	List<Despesas> despesas = repositorio.findAll();
 	model.addAttribute("despesas", despesas);
 	
-	
-	
-	List<Double> valoresNd04 = new ArrayList<>();
-	List<Double> valoresNd30 = new ArrayList<>();
-	List<Double> valoresNd33 = new ArrayList<>();
-	List<Double> valoresNd36 = new ArrayList<>();
-	List<Double> valoresNd39 = new ArrayList<>();
-	List<Double> valoresNd40 = new ArrayList<>();
-	List<Double> valoresNd52 = new ArrayList<>();
-	
-	
+			
 	
 	List<Double> valoresJaneiro = new ArrayList<>();
 	List<Double> valoresFevereiro = new ArrayList<>();
@@ -131,7 +207,6 @@ public class DespesasController {
 	
 	
 	
-	
 	//metodo para cadastrar qed	
 		@PostMapping("/cadastrarDespesa")
 		public String cadastroDespesa(@ModelAttribute Despesas despesas,
@@ -152,6 +227,11 @@ public class DespesasController {
 		List<Despesas> despesas = repositorio.findAll();
 		model.addAttribute("despesas", despesas);
 		return "editarLista";
+		}
+		
+		@PostMapping("despesas/editar")
+		public String editarDespesa(Despesas despesa) {
+			return "redirect:/despesas";
 		}
 
 		
